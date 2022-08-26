5972595d929966b0429f6f0f560caaa0bb04850f
Muhammad Shafie <shafiehb@gmail.com>
2022-08-26 23:04:36 +0800

    build project and add route transition

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,26 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: "/weather/:name/:country",
       name: "cityView",
       component: CityView,
+      meta: {
+        title: "Weather",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.name ? `${to.params.name}, ${to.params.country}` : to.meta.title
+  } | The Local Weather`;
+  next();
 });
 
 export default router;
